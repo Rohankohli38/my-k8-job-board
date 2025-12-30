1f1d95d02b6289a5354dc2443457d3ff1b5f146e
Ubuntu <ubuntu@ip-172-31-25-26.ec2.internal>
2025-12-30 05:37:55 +0000

    Puhsing to github

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 # backend.tf
 terraform {
   backend "s3" {
-    bucket         = "job-board-terraform-state-180048382895"  # Create this bucket first
+    bucket         = "job-board-terraform-state-556789079263"  # Create this bucket first
     key            = "eks/terraform.tfstate"
-    region         = "eu-west-2"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "job-board-terraform-state-lock"       # Create this table first
   }
